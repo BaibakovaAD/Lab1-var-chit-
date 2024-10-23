4a9ddf9656210a3b8e945eb6f17b3a86fc314709
BaibakovaAD <nastya.bay.ry@gmail.com>
2024-10-23 22:45:37 +0500

    Create Fraction.java

---
+++ Fraction.java
@@ -0,0 +1,94 @@
+public class Fraction {
+    private int numerator;   // Числитель
+    private int denominator; // Знаменатель
+
+    // Конструктор, принимающий числитель и знаменатель
+    public Fraction(int numerator, int denominator) {
+        if (denominator == 0) {
+            throw new IllegalArgumentException("Знаменатель не может быть равен нулю.");
+        }
+        this.numerator = numerator;
+        this.denominator = denominator;
+        simplify(); // Сокращаем дробь при создании
+    }
+
+    // Метод для получения строкового представления дроби
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+
+    // Метод для сложения двух дробей
+    public Fraction add(Fraction other) {
+        int newNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    // Метод для вычитания двух дробей
+    public Fraction subtract(Fraction other) {
+        int newNumerator = this.numerator * other.denominator - other.numerator * this.denominator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    // Метод для умножения двух дробей
+    public Fraction multiply(Fraction other) {
+        int newNumerator = this.numerator * other.numerator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    // Метод для деления двух дробей
+    public Fraction divide(Fraction other) {
+        if (other.numerator == 0) {
+            throw new ArithmeticException("Нельзя делить на ноль.");
+        }
+        int newNumerator = this.numerator * other.denominator;
+        int newDenominator = this.denominator * other.numerator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    // Метод для сложения дроби с целым числом
+    public Fraction add(int number) {
+        return add(new Fraction(number, 1));
+    }
+
+    // Метод для вычитания целого числа из дроби
+    public Fraction subtract(int number) {
+        return subtract(new Fraction(number, 1));
+    }
+
+    // Метод для умножения дроби на целое число
+    public Fraction multiply(int number) {
+        return multiply(new Fraction(number, 1));
+    }
+
+    // Метод для деления дроби на целое число
+    public Fraction divide(int number) {
+        return divide(new Fraction(number, 1));
+    }
+
+    // Упрощение дроби (сокращение)
+    private void simplify() {
+        int gcd = gcd(Math.abs(numerator), Math.abs(denominator));
+        numerator /= gcd;
+        denominator /= gcd;
+
+        // Обеспечиваем, что только числитель может быть отрицательным
+        if (denominator < 0) {
+            numerator = -numerator;
+            denominator = -denominator;
+        }
+    }
+
+    // Нахождение наибольшего общего делителя
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
